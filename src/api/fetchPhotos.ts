46dd07d317495f6edf99e94f62a01bcca2280eb9
FlorianChauvel <chauvelflorian@gmail.com>
2021-07-26 17:44:33 +0200

    feat: 🎸 switch from 20 to 24 pictures per page

--- src/api/fetchPhotos.ts
+++ src/api/fetchPhotos.ts
@@ -2,7 +2,7 @@ import { FetchPhotosResponse } from "../types/FetchPhotosResponse";
 import { API_KEY, API_URL } from "./constants";
 
 export const fetchPhotos = async (page: number): Promise<FetchPhotosResponse> => {
-    const perPage = 20;
+    const perPage = 24;
     const extras = 'url_s,url_m,url_l,owner_name';
     const url = `${API_URL}/?method=flickr.photos.getRecent&api_key=${API_KEY}&format=json&nojsoncallback=1&extras=${extras}&per_page=${perPage}&page=${page}`;
     
