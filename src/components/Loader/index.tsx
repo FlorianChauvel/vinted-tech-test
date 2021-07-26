a09f2e1a8062d131d688eefbb543c29486cb6fd4
FlorianChauvel <chauvelflorian@gmail.com>
2021-07-26 19:18:43 +0200

    fix: 🐛 stroke-width => strokeWidth

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -3,7 +3,7 @@ import './Loader.css';
 const Loader: React.FC = () => (
     <div className="loader">
         <svg className="spinner" viewBox="0 0 50 50">
-            <circle className="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+            <circle className="path" cx="25" cy="25" r="20" fill="none" strokeWidth="5"></circle>
         </svg>
     </div>
 );
